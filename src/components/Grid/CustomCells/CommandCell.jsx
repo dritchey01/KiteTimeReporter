57e39d98acc2d87bdf88e91842d55ee8d8a89360
dritchey01 <david.ritchey@ku.edu>
2023-11-22 13:01:26 -0500

    build cell for CRUD actions

---
+++ src/components/Grid/CustomCells/CommandCell.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+export const CommandCell = ({ dataItem, editField, add, update, edit, discard, cancel, remove }) => {
+  const inEdit = dataItem[editField];
+  const isNewItem = dataItem.id === undefined;
+  return inEdit ? <td className="k-command-cell">
+      <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-save-command" onClick={() => isNewItem ? add(dataItem) : update(dataItem)}>
+        {isNewItem ? "Add" : "Update"}
+      </button>
+      <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-cancel-command" onClick={() => isNewItem ? discard(dataItem) : cancel(dataItem)}>
+        {isNewItem ? "Discard" : "Cancel"}
+      </button>
+    </td> : <td className="k-command-cell">
+      <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary k-grid-edit-command" onClick={() => edit(dataItem)}>
+        Edit
+      </button>
+      <button className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-base k-grid-remove-command" onClick={() => confirm("Confirm deleting Row? ") && remove(dataItem)}>
+        Remove
+      </button>
+    </td>;
+};
+
+CommandCell.propTypes = {
+  dataItem: PropTypes.object,
+  editField: PropTypes.string,
+  add: PropTypes.func,
+  update: PropTypes.func,
+  edit: PropTypes.func,
+  discard: PropTypes.func,
+  cancel: PropTypes.func,
+  remove: PropTypes.func,
+
+}
