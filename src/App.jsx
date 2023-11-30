7a5b5f37c4e1bca710876aab55d403d38ada2446
Dj Ritchey <david.ritchey@ku.edu>
2023-11-30 11:51:23 -0500

    correct formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import './styles/base/index.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './components/Header'
-import AllRoutes from './Routers/routes';
-import { useState } from 'react';
+import "./styles/base/index.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+import AllRoutes from "./Routers/routes";
+import { useState } from "react";
 
 const App = () => {
-  const [isAdmin, setIsAdmin] = useState(false)
+  const [isAdmin, setIsAdmin] = useState(false);
 
   return (
     <Router>
       <Header setIsAdmin={setIsAdmin} />
-          <AllRoutes isAdmin={isAdmin} />
+      <AllRoutes isAdmin={isAdmin} />
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
