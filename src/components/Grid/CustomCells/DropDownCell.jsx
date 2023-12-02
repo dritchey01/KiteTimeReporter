d58bb12aa6b3f7a29315b63c947e695852d2269f
Dj Ritchey <david.ritchey@ku.edu>
2023-12-01 23:59:24 -0500

    add on change

--- src/components/Grid/CustomCells/DropDownCell.jsx
+++ src/components/Grid/CustomCells/DropDownCell.jsx
@@ -1,12 +1,28 @@
 import PropTypes from "prop-types";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
 
-export const DropDownCell = ({ dataItem, editField, field }) => {
-  const inEdit = dataItem[editField];
-  const isNewItem = dataItem.id === undefined;
-  return inEdit ? (
+export const DropDownCell = ({ dataItem, field, list, onChange }) => {
+  const handleChange = (e) => {
+    if (onChange) {
+      onChange({
+        dataIndex: 0,
+        dataItem: dataItem,
+        field: field,
+        syntheticEvent: e.syntheticEvent,
+        value: e.target.value,
+      });
+    }
+  };
+
+  const dataValue = dataItem[field] === null ? "" : dataItem[field];
+
+  return dataItem.inEdit ? (
     <td className="k-command-cell">
-      <DropDownList data={["a", "b", "c", "d", "e", "f"]} />
+      <DropDownList
+        data={list}
+        onChange={handleChange}
+        value={list.find((str) => str === dataValue)}
+      />
     </td>
   ) : (
     <td className="k-command-cell">{dataItem[field]}</td>
@@ -17,4 +33,6 @@ DropDownCell.propTypes = {
   dataItem: PropTypes.object,
   editField: PropTypes.string,
   field: PropTypes.string,
+  list: PropTypes.array,
+  onChange: PropTypes.func,
 };
