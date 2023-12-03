4e966044bf62a9e4ea6e4234ff9787caa42b29b2
Dj Ritchey <david.ritchey@ku.edu>
2023-12-03 08:03:25 -0500

    base for user selection before showing grid

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,33 @@
-import ReportGrid from '../components/Grid/index'
+import { useState } from "react";
+import ReportGrid from "../components/Grid/index";
+import { DropDownList } from "@progress/kendo-react-dropdowns";
 
 const Home = () => {
+  const [selectedUser, setSelectedUser] = useState(null);
+  const users = [
+    "Dj Ritchey",
+    "Andy Greenhaw",
+    "Fethi Akcay",
+    "Mahesh Chandana",
+  ];
+  const handleChange = (e) => {
+    setSelectedUser(e.target.value);
+  };
 
   return (
     <div>
-      <ReportGrid />
+      <div className="grid grid-cols-8 p-2">
+        <div className="col-span-1">Please select your Name: </div>
+        <DropDownList
+          data={users}
+          onChange={handleChange}
+          value={users.find((str) => str === selectedUser)}
+          className="col-span-2"
+        />
+      </div>
+      {selectedUser && <ReportGrid selectedUser={selectedUser} />}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
