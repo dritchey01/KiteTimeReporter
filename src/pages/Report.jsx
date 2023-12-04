59a237391e7637fc9fa7582341eebbdcc64a38bb
Dj Ritchey <david.ritchey@ku.edu>
2023-12-04 11:27:17 -0500

    add button for report generation

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import { DropDownList } from "@progress/kendo-react-dropdowns";
 import { DateRangePicker } from "@progress/kendo-react-dateinputs";
+import { Button } from "@progress/kendo-react-buttons";
 
 const ReportPage = ({ users }) => {
   const [empRowData, setEmpRowData] = useState([
@@ -13,7 +14,7 @@ const ReportPage = ({ users }) => {
       contract: "ATLAS",
       taskName: "Create New Maps",
       hours: "20",
-      workWeek: "2023-11-01",
+      workWeek: "2023-W26",
       description: "Created a new Map to demo for ATLAS",
     },
     {
@@ -24,7 +25,7 @@ const ReportPage = ({ users }) => {
       contract: "ATLAS",
       taskName: "Fix QA Defects",
       hours: "28",
-      workWeek: "2023-11-02",
+      workWeek: "2023-W26",
       description: "Address new defect list",
     },
     {
@@ -35,7 +36,7 @@ const ReportPage = ({ users }) => {
       contract: "ATLAS",
       taskName: "Design workflow",
       hours: "6",
-      workWeek: "2023-11-04",
+      workWeek: "2023-W26",
       description: "Create new working doc for workflow in LM",
     },
   ]);
@@ -55,6 +56,10 @@ const ReportPage = ({ users }) => {
     label: null,
   };
 
+  const calendarSettings = {
+    weekNumber: true,
+  };
+
   return (
     <>
       To Generate a report for a user across all weeks select user and submit,
@@ -63,12 +68,19 @@ const ReportPage = ({ users }) => {
         <div className="basis-1/4 p-2">
           Please select user for report: <DropDownList data={users} />
         </div>
-        <div className="basis-1/4 p-2">
+        <div className="basis-1/6 p-2">
           Please select date range for report:{" "}
           <DateRangePicker
             startDateInputSettings={dateInputSettings}
             endDateInputSettings={dateInputSettings}
+            max={new Date()}
+            calendarSettings={calendarSettings}
           />
+        </div>
+        <div className="py-8">
+          <Button style={{ backgroundColor: "#1560b7", color: "white" }}>
+            Show Report
+          </Button>
         </div>
       </div>
     </>
