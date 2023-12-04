f07b3ca6a8d11cf1a47be5dea577675856622aca
Dj Ritchey <david.ritchey@ku.edu>
2023-12-04 10:56:13 -0500

    pass through props

--- src/Routers/routes.jsx
+++ src/Routers/routes.jsx
@@ -2,20 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import Report from "../pages/Report";
 import Home from "../pages/Home";
 import ErrorPage from "../pages/Error";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const AllRoutes = ({ isAdmin }) => {
+const AllRoutes = ({ users }) => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/report" element={<Report isAdmin={isAdmin} />}/>
-      <Route path="*" element={<ErrorPage />}/>
+      <Route path="/" element={<Home users={users} />} />
+      <Route path="/report" element={<Report users={users} />} />
+      <Route path="*" element={<ErrorPage />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
 
 AllRoutes.propTypes = {
-  isAdmin: PropTypes.bool
-}
+  isAdmin: PropTypes.bool,
+  users: PropTypes.array,
+};
